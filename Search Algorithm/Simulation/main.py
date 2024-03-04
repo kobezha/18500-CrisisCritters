7d2e3bdf4a27f06873b07e222f0324352ad7d97b
CasperWong-jpg <casper.wong.001@hotmail.com>
2024-03-04 11:59:59 -0500

    Working without optimize

--- Search Algorithm/Simulation/main.py
+++ Search Algorithm/Simulation/main.py
@@ -27,7 +27,6 @@ def print_grid(grid: List[List[SquareType]]):
     for col in range(len(grid[0]) + 2):
         print(SquareType.BLOCKED.value, end=" ")
     print()
-        
 
 
 def change_squares(grid: List[List[SquareType]], orientation: Orientation, offset: int, 
@@ -40,7 +39,7 @@ def change_squares(grid: List[List[SquareType]], orientation: Orientation, offse
             grid[row][offset] = square_type
 
 
-def generate_building_grid() -> List[List[SquareType]]:
+def generate_building_grid(numHexapods = 1) -> List[List[SquareType]]:
     """
     Creating map specified in https://ieeexplore.ieee.org/document/1389384
     Assume edges of grid are walls (BLOCKED)
@@ -79,48 +78,63 @@ def generate_building_grid() -> List[List[SquareType]]:
     building[half+1][2 * third + 1] = SquareType.TARGET
     
     # Add Hexapods
-    building[5 * sixth][sixth] = SquareType.HEXAPOD
+    for i in range(numHexapods):
+        # Deploy Hexapods scattered in left-bottom room
+        while True:
+            row, col = random.choice(range(2 * third + 1, num_squares)), random.choice(range(1, third))
+            if building[row][col] == SquareType.EMPTY:
+                building[row][col] = SquareType.HEXAPOD
+                break
 
     return building
 
 
-def find_hexapods(building: List[List[SquareType]]) -> List[Tuple[int, int]]:
+def find_obj(building: List[List[SquareType]], obj: SquareType) -> List[Tuple[int, int]]:
     res = []
     for row in range(len(building)):
         for col in range(len(building[0])):
-            if building[row][col] == SquareType.HEXAPOD:
+            if building[row][col] == obj:
                 res.append((row, col))
     return res
 
 
-def simulate_step(building: List[List[SquareType]], optimize: bool = False) -> None:
+def simulate_step(building: List[List[SquareType]], optimize: bool = False) -> int:
     """
-    optimize: avoid revisiting previously visited nodes
-    # TODO: Walk away from other hexapods
+    Optimize: avoid revisiting previously visited nodes
+    TODO: Implement
+    return: Number of targets found in this simulation step
     """
     directions = [(i, j) for i in range(-1, 2) for j in range(-1, 2)]
-    directions.remove((0, 0))
     rows, cols = len(building), len(building[0])
-    hexapods = find_hexapods(building)
+    hexapods = find_obj(building, SquareType.HEXAPOD)
+    found = 0
     for hexapod in hexapods:
         row, col = hexapod
         while True:
             drow, dcol = random.choice(directions)
             new_row, new_col = row + drow, col + dcol
-            if (0 <= new_row < rows and 0 <= new_col < cols and 
-                building[new_row][new_col] in [SquareType.EMPTY, SquareType.VISITED]):
-                # Unoccupied square, feel free to enter
-                break
+            if 0 <= new_row < rows and 0 <= new_col < cols:
+                if optimize...:
+                if building[new_row][new_col] in [SquareType.EMPTY, SquareType.VISITED]:
+                    # Unoccupied square, feel free to enter
+                    break
+                if building[new_row][new_col] == SquareType.TARGET:
+                    # TODO: Notify Medbot in real simulation
+                    found += 1
+                    break
         building[new_row][new_col] = SquareType.HEXAPOD
         building[row][col] = SquareType.VISITED
-        # TODO: Check if a target
-    return
+    return found
 
 
 if __name__ == '__main__':
-    building = generate_building_grid()
+    building = generate_building_grid(3)
+    print("Original map:")
     print_grid(building)
-    for step in range(500):
+    num_steps = 0
+    while find_obj(building, SquareType.TARGET):
         simulate_step(building)
+        num_steps += 1
+    print(f"Number of steps taken: {num_steps}")
     print_grid(building)
 
