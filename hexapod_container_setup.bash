825d60d8510d5c53c7028182becae4030a1d1991
Kobe <majorkobe@gmail.com>
2024-03-31 18:38:25 -0400

    hexapod_container_setup.bash

---
+++ hexapod_container_setup.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip3 install pyrealsense2
+pip3 install colcon-clean
+sudo apt-get install -y ros-humble-isaac-ros-yolov8 ros-humble-isaac-ros-tensor-rt ros-humble-isaac-ros-dnn-image-encoder
+sudo apt-get remove -y ros-humble-isaac-ros-yolov8
+sudo apt install x11-apps
+sudo apt install tmux
